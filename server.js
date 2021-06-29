0924adc4b99452fda6b778c0a90a14c66e5f4d7c
ranul <ranul.jaya@gmail.com>
2021-06-30 04:44:28 +1200

    change in the production env for the server

--- server.js
+++ server.js
@@ -1,38 +1,34 @@
 // Importing Modules
-const mongoose = require('mongoose');
-const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose')
+const express = require('express')
+const path = require('path')
 
 require('dotenv').config()
 
 // importing files
-const routes = require('./routes/api');
+const routes = require('./routes/api')
 
 // Define Global Variables
-const app = express();
-const log = console.log;
-const PORT = process.env.PORT || 8080; // Step 1
+const app = express()
+const log = console.log
+const PORT = process.env.PORT || 8080 // Step 1
 
 // Step 2
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/my_database', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/my_database', {
     useNewUrlParser: true,
-    useUnifiedTopology: true 
-});
+    useUnifiedTopology: true,
+})
 
 // Configuration
 app.use(express.json())
 app.use(express.urlencoded())
-app.use('/api', routes);
+app.use('/api', routes)
 
 // Step 3
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( 'client/build' ));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
-    });
+    app.use(express.static('client/build'))
 }
 
 app.listen(PORT, () => {
-    log(`Server is starting at PORT: ${PORT}`);
-});
+    log(`Server is starting at PORT: ${PORT}`)
+})
