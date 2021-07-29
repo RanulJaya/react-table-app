884820a58bcaf6b59999df834b1c35889f31e961
RanulJaya <49587910+RanulJaya@users.noreply.github.com>
2021-07-29 15:11:32 +1200

    first commit

--- routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const data = require('../data/db.json')
-
-router.get('/books', (req, res) => {
-    res.json(data)
-})
-
-module.exports = router;
